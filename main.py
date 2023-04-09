30cc57e27dd34db4ae5e8a31221c1507c76f6af0
Artizeus <mitchell.sweet@aol.com>
2023-04-08 19:53:04 -0500

    update

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn import linear_model
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+from bioinfokit.analys import stat
 
 statsFile = ("hockeyStats.csv")#File with team stats
 seasonFile = ("seasonResults.csv")#File with all games and results
@@ -27,6 +31,35 @@ gameData = pd.read_csv(seasonFile)
 
 #Function responsible for comparing the stats of teams in a given game
 #def readGame(hTeam, aTeam, hScore, aScore):
-    
 
-print (statData)
+pmDiff = []
+for x, y in zip(statData['PIM'], statData['OppPIM']):
+    pmDiff.append(y-x)
+
+statData = statData.assign(PIMDiff = pmDiff)
+#print(statData)
+res = stat()
+res.anova_stat(df=statData, res_var='G', anova_model='G ~ C(PIMDiff)')
+print(res.anova_summary)
+
+w, pvalue = stats.shapiro((ols('G ~ C(PIMDiff)', data=statData).fit()).resid)
+print(w, pvalue)
+
+#sm.qqplot(res.anova_std_residuals, line='45')
+#plt.show()
+
+#plt.scatter(pmDiff, statData["G"])
+#plt.show()
+def find_outliers_IQR(df):
+
+   q1=df.quantile(0.25)
+
+   q3=df.quantile(0.75)
+
+   IQR=q3-q1
+
+   outliers = df[((df<(q1-1.5*IQR)) | (df>(q3+1.5*IQR)))]
+
+   return outliers
+
+#print (statData)
